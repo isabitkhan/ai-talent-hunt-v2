cbdc1d41801a9b75e06e8c8c7fe3dbecf53b1d5a
isabitkhan <mesabitkhan@gmail.com>
2026-08-09 00:53:25 +0500

    add ? route

--- backend/server.js
+++ backend/server.js
@@ -66,6 +66,9 @@ app.use((err, req, res, next) => {
   console.error(err.stack);
   res.status(500).json({ message: err.message || "Something went wrong!" });
 });
+app.get("/", (req, res) => {
+  res.json({ message: "AI Talent Hunt API is running!" });
+});
 
 const PORT = process.env.PORT || 5000;
 module.exports = app;
